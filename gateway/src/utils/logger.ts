a74b8227fafdf761ce583c1dc4a858ca051fc3aa
Siddhi sahu <himanisahu739@gmail.com>
2026-09-23 17:54:04 +0530

    Add logger utility with methods

--- gateway/src/utils/logger.ts
+++ gateway/src/utils/logger.ts
@@ -1,12 +1,21 @@
+
 type LogLevel = "info" | "warn" | "error";
 
+interface LogData {
+    [key: string] : unknown
+}
 
-function log(level: LogLevel, event: string){
+function log(level: LogLevel, event: string, data: LogData = {}){
     console.log(JSON.stringify({
         timestamp: new Date().toISOString(),
         level,
-        event
+        event,
+        ...data
     }))
 }
 
-// export const logger
+export const logger = {
+    info: (event: string, data?: LogData ) => log("info", event, data),
+    warn: (event: string, data?: LogData) => log("warn", event, data),
+    error: (event: string, data?: LogData) => log("error", event, data)
+}
